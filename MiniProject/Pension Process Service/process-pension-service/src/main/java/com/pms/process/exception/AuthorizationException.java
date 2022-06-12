4abfc21d15ad180a0abca6876508f17568fbd105
Srikar458 <105718812+Srikar458@users.noreply.github.com>
2022-06-12 12:34:02 +0000

    Delete MiniProject/Pension Process Service/process-pension-service directory

--- MiniProject/Pension Process Service/process-pension-service/src/main/java/com/pms/process/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.pms.process.exception;
-
-@SuppressWarnings("serial")
-public class AuthorizationException extends Exception{
-
-	public AuthorizationException(String message) {
-		super(message);
-	}
-}
